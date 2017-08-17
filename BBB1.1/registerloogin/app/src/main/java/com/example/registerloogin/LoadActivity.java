6933c831cd586ebbf549612df4b155a62c8ec259
lenovo-PC\lenovo <549017210@qq.com>
2017-08-17 13:48:28 +0800

    QQ登录

--- BBB1.1/registerloogin/app/src/main/java/com/example/registerloogin/LoadActivity.java
+++ BBB1.1/registerloogin/app/src/main/java/com/example/registerloogin/LoadActivity.java
@@ -2,6 +2,7 @@ package com.example.registerloogin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.tencent.open.utils.Util;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -28,6 +35,7 @@ public class LoadActivity extends AppCompatActivity  {
 
     private TextView textView;
     private Button buttonLogin;
+    private Button buttonQQLogin;
     private Button buttonRegister;
     private EditText editUsername;
     private EditText editPassword;
@@ -35,6 +43,8 @@ public class LoadActivity extends AppCompatActivity  {
     private String msg;
     //private String data;
     //private String token;
+    private Tencent mTencent;
+    private static boolean isServerSideLogin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +61,8 @@ public class LoadActivity extends AppCompatActivity  {
         buttonLogin.setOnClickListener(new MyListener());
         buttonRegister = (Button) findViewById(R.id.btn_register);
         buttonRegister.setOnClickListener(new MyListener());
+        buttonQQLogin = (Button) findViewById(R.id.btn_qqlogin);
+        buttonQQLogin.setOnClickListener(new MyListener());
     }
 
 
@@ -116,6 +128,71 @@ public class LoadActivity extends AppCompatActivity  {
         }
     }
 
+    IUiListener loginListener = new BaseUiListener() {
+
+    };
+
+
+    private void qqLogin(){
+        mTencent = Tencent.createInstance("1106260747", this.getApplicationContext());
+        if (!mTencent.isSessionValid()) {
+            mTencent.login(this, "all", loginListener);
+            isServerSideLogin = false;
+            Log.d("SDKQQAgentPref", "FirstLaunch_SDK:" + SystemClock.elapsedRealtime());
+        } else {
+            if (isServerSideLogin) { // Server-Side 模式的登陆, 先退出，再进行SSO登陆
+                mTencent.logout(this);
+                mTencent.login(this, "all", loginListener);
+                isServerSideLogin = false;
+                Log.d("SDKQQAgentPref", "FirstLaunch_SDK:" + SystemClock.elapsedRealtime());
+                return;
+            }
+            mTencent.logout(this);
+           // updateUserInfo();
+            // updateLoginButton();
+        }
+    }
+
+    private class BaseUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object response) {
+            if (null == response) {
+              //  Util.showResultDialog(LoadActivity.this, "返回为空", "登录失败");
+                return;
+            }
+            JSONObject jsonResponse = (JSONObject) response;
+            if (null != jsonResponse && jsonResponse.length() == 0) {
+              //  Util.showResultDialog(LoadActivity.this, "返回为空", "登录失败");
+                return;
+            }
+          //  Util.showResultDialog(LoadActivity.this, response.toString(), "登录成功");
+            // 有奖分享处理
+           // handlePrizeShare();
+            doComplete((JSONObject)response);
+
+        }
+
+        protected void doComplete(JSONObject values) {
+            Intent m_intent = new Intent(LoadActivity.this, HomePageActivity.class);
+            startActivity(m_intent);
+        }
+
+        @Override
+        public void onError(UiError e) {
+           /*  Util.toastMessage(LoadActivity.this, "onError: " + e.errorDetail);
+            Util.dismissDialog();*/
+        }
+
+        @Override
+        public void onCancel() {
+           /* Util.toastMessage(LoadActivity.this, "onCancel: ");
+            Util.dismissDialog();
+            if (isServerSideLogin) {
+                isServerSideLogin = false;
+            }*/
+        }
+    }
 
     public class MyListener implements View.OnClickListener {
         public void onClick(View v) {
@@ -128,8 +205,15 @@ public class LoadActivity extends AppCompatActivity  {
                     Intent intent = new Intent(LoadActivity.this, RegisterActivity.class);
                     startActivity(intent);
                     break;
+                case  R.id.btn_qqlogin:
+                    qqLogin();
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Tencent.onActivityResultData(requestCode,resultCode,data,loginListener);
     }
 
 }
