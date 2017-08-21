c3240d2df2b12cfb74ff1bdf1951aeb64fdc4644
lenovo-PC\lenovo <549017210@qq.com>
2017-08-21 12:58:55 +0800

    QQ登录2

--- BBB1.1/registerloogin/app/src/main/java/com/example/registerloogin/LoadActivity.java
+++ BBB1.1/registerloogin/app/src/main/java/com/example/registerloogin/LoadActivity.java
@@ -13,12 +13,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.connect.common.Constants;
 import com.tencent.open.utils.Util;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
 import com.tencent.tauth.UiError;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import okhttp3.FormBody;
@@ -45,6 +49,12 @@ public class LoadActivity extends AppCompatActivity  {
     //private String token;
     private Tencent mTencent;
     private static boolean isServerSideLogin = false;
+    private String openid;
+    private String qqtoken;
+    private String expires;
+    private String nickname;
+    private String phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,7 +122,7 @@ public class LoadActivity extends AppCompatActivity  {
                             break;
                         case "200":
                             //Toast.makeText(LoadActivity.this, "登录成功！", Toast.LENGTH_SHORT).show();
-                            String phone = editUsername.getText().toString();
+                            phone = editUsername.getText().toString();
                             //Intent intent = new Intent(LoadActivity.this, OrderPageActivity.class);
                             //intent.putExtra("extra_token", token);
                             Intent m_intent = new Intent(LoadActivity.this, HomePageActivity.class);
@@ -174,8 +184,73 @@ public class LoadActivity extends AppCompatActivity  {
         }
 
         protected void doComplete(JSONObject values) {
-            Intent m_intent = new Intent(LoadActivity.this, HomePageActivity.class);
-            startActivity(m_intent);
+            //设置openid和token，否则获取不到下面的信息
+            initOpenidAndToken(values);
+            //获取QQ用户的各信息
+            getUserInfo();
+            //submitQQInformation();
+        }
+
+        private void submitQQInformation() {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        OkHttpClient client = new OkHttpClient();
+                        RequestBody requestBody = new FormBody.Builder()
+                                .add("openid",openid)
+                                .add("name",nickname)
+                                .build();
+                        Request request = new Request.Builder()
+                                .addHeader("Content-Type", "application/json")
+                                .url("http://bang.cloudshm.com/registerAndLogin/qqLogin")
+                                .post(requestBody)
+                                .build();
+                        Response response = client.newCall(request).execute();
+                        String responseData = response.body().string();
+                        judgeFirstQQLogin(responseData);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+        private  void judgeFirstQQLogin(String jsonData){
+            try{
+                JSONObject jsonObject = new JSONObject(jsonData);
+                status = jsonObject.getString("status");
+                msg = jsonObject.getString("msg");
+                phone = jsonObject.getString("phone");
+
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (msg) {
+                            case "firstQQLogin successfully":
+                                Toast.makeText(LoadActivity.this, "首次登陆！", Toast.LENGTH_SHORT).show();
+                                Intent n_intent = new Intent(LoadActivity.this, QQRegisterActivity.class);
+                                n_intent.putExtra("extra_openid", openid);
+                                startActivity(n_intent);
+                                break;
+                            case "qqLogin successfully":
+                                Toast.makeText(LoadActivity.this, "二次登录！", Toast.LENGTH_SHORT).show();
+                                Intent m_intent = new Intent(LoadActivity.this, HomePageActivity.class);
+                                m_intent.putExtra("extra_phone", phone);
+                                startActivity(m_intent);
+                                break;
+                            case "need openid":
+                                Toast.makeText(LoadActivity.this, "参数缺失！", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -215,5 +290,104 @@ public class LoadActivity extends AppCompatActivity  {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Tencent.onActivityResultData(requestCode,resultCode,data,loginListener);
     }
+
+    private void initOpenidAndToken(JSONObject jsonObject) {
+        try {
+            openid = jsonObject.getString("openid");
+            qqtoken = jsonObject.getString("access_token");
+            expires = jsonObject.getString("expires_in");
+
+            mTencent.setAccessToken(qqtoken, expires);
+            mTencent.setOpenId(openid);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private IUiListener userInfoListener = new IUiListener() {
+        @Override
+        public void onComplete(final Object o) {
+            JSONObject userInfoJson = (JSONObject) o;
+            try {
+                nickname = userInfoJson.getString("nickname");//直接传递一个昵称的内容过去
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            OkHttpClient client = new OkHttpClient();
+                            RequestBody requestBody = new FormBody.Builder()
+                                    .add("openid",openid)
+                                    .add("name",nickname)
+                                    .build();
+                            Request request = new Request.Builder()
+                                    .addHeader("Content-Type", "application/json")
+                                    .url("http://bang.cloudshm.com/registerAndLogin/qqLogin")
+                                    .post(requestBody)
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            String responseQQData = response.body().string();
+                            try{
+                                JSONObject jsonObject = new JSONObject(responseQQData);
+                                status = jsonObject.getString("status");
+                                msg = jsonObject.getString("msg");
+                                phone = jsonObject.getString("phone");
+
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(LoadActivity.this, status, Toast.LENGTH_LONG).show();
+                                        Toast.makeText(LoadActivity.this, msg + nickname, Toast.LENGTH_LONG).show();
+                                        switch (msg) {
+                                            case "firstQQLogin successfully":
+                                                Toast.makeText(LoadActivity.this, "首次登陆！", Toast.LENGTH_SHORT).show();
+                                                Intent n_intent = new Intent(LoadActivity.this, QQRegisterActivity.class);
+                                                n_intent.putExtra("extra_openid", openid);
+                                                startActivity(n_intent);
+                                                break;
+                                            case "qqLogin successfully":
+                                                Toast.makeText(LoadActivity.this, "二次登录！", Toast.LENGTH_SHORT).show();
+                                                Intent m_intent = new Intent(LoadActivity.this, HomePageActivity.class);
+                                                m_intent.putExtra("extra_phone", phone);
+                                                startActivity(m_intent);
+                                                break;
+                                            case "need openid":
+                                                Toast.makeText(LoadActivity.this, "参数缺失！", Toast.LENGTH_SHORT).show();
+                                                break;
+                                        }
+                                    }
+                                });
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onError(UiError uiError) {
+        }
+        @Override
+        public void onCancel() {
+        }
+    };
+
+
+        private void getUserInfo() {
+        //sdk给我们提供了一个类UserInfo，这个类中封装了QQ用户的一些信息，我么可以通过这个类拿到这些信息
+        QQToken mQQToken = mTencent.getQQToken();
+        UserInfo userInfo = new UserInfo(LoadActivity.this, mQQToken);
+        userInfo.getUserInfo(userInfoListener);
+            Toast.makeText(LoadActivity.this, nickname+openid, Toast.LENGTH_LONG).show();
+
+        }
+
 
 }
